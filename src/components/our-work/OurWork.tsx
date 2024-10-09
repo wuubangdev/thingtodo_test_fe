81a15a7ca058f6274bf596b0f9bdeb93a763ae29
wuubangdev <wuubangdev@gmail.com>
2024-10-09 21:41:38 +0700

    our work update

--- src/components/our-work/OurWork.tsx
+++ src/components/our-work/OurWork.tsx
@@ -62,6 +62,15 @@ const OurWork = () => {
                     />
                 ))}
             </div>
+            <div className='grid grid-cols-1 md:grid-cols-2'>
+                <div></div>
+                <div className='flex gap-4 items-center cursor-pointer'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="md:size-10">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                    </svg>
+                    <span className='text-custom-phone'>See all projects</span>
+                </div>
+            </div>
         </div>
     )
 }
