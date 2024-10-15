2606d8bdaf449dd55a59ec8cafe81327685a4f7b
wuubangdev <wuubangdev@gmail.com>
2024-10-15 12:54:00 +0700

    fix deloy maquee

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -17,8 +17,9 @@ const Feature = () => {
             direction='left'
             pauseOnHover={true}
             style={{ cursor: 'pointer' }}
+            loop={0}
         >
-            <div key={1} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between'>
+            <div key={1} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between w-full'>
                 {items && items.map((item, index) => (
                     <div key={`left-${index}`} className='flex gap-2 text-2xl items-center justify-evenly'>
                         <p className='text-nowrap'>{item}</p>
@@ -49,7 +50,7 @@ const Feature = () => {
                     </div>
                 ))}
             </div>
-            <div key={2} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between'>
+            <div key={2} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between w-full'>
                 {items && items.map((item, index) => (
                     <div key={`left-${index}`} className='flex gap-2 text-2xl items-center justify-evenly'>
                         <p className='text-nowrap'>{item}</p>
@@ -80,9 +81,40 @@ const Feature = () => {
                     </div>
                 ))}
             </div>
-            <div key={3} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between'>
+            <div key={3} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between w-full'>
                 {items && items.map((item, index) => (
-                    <div key={index} className='flex gap-2 text-2xl items-center justify-evenly'>
+                    <div key={`left-${index}`} className='flex gap-2 text-2xl items-center justify-evenly'>
+                        <p className='text-nowrap'>{item}</p>
+                        <div className='h-1/2 md:block hidden'>
+                            <Image
+                                alt='shaking'
+                                src={'/hero/Mobile_shaking.svg'}
+                                width={17}
+                                height={17}
+                                style={{
+                                    width: "auto",
+                                    height: "100%"
+                                }}
+                            />
+                        </div>
+                        <div className='md:hidden flex'>
+                            <Image
+                                alt='shaking'
+                                src={'/hero/mobile_feature_icon.svg'}
+                                width={17}
+                                height={17}
+                                style={{
+                                    width: "100%",
+                                    height: "auto"
+                                }}
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div key={4} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between w-full'>
+                {items && items.map((item, index) => (
+                    <div key={`left-${index}`} className='flex gap-2 text-2xl items-center justify-evenly'>
                         <p className='text-nowrap'>{item}</p>
                         <div className='h-1/2 md:block hidden'>
                             <Image
