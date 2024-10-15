3221dc8483989db3a2cdac2ff98fdf43c6e1f4b3
wuubangdev <wuubangdev@gmail.com>
2024-10-15 12:59:52 +0700

    fix deloy maquee2

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -11,110 +11,19 @@ const items: string[] = [
 ]
 
 const Feature = () => {
+    const duplicatedItems = [...items, ...items]; // Nhân đôi các mục
+
     return (
         <Marquee
             speed={50}
             direction='left'
             pauseOnHover={true}
+            loop={0} // Số lần lặp lại. Sử dụng `0` để lặp vô hạn.
             style={{ cursor: 'pointer' }}
-            loop={0}
         >
-            <div key={1} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between w-full'>
-                {items && items.map((item, index) => (
-                    <div key={`left-${index}`} className='flex gap-2 text-2xl items-center justify-evenly'>
-                        <p className='text-nowrap'>{item}</p>
-                        <div className='h-1/2 md:block hidden'>
-                            <Image
-                                alt='shaking'
-                                src={'/hero/Mobile_shaking.svg'}
-                                width={17}
-                                height={17}
-                                style={{
-                                    width: "auto",
-                                    height: "100%"
-                                }}
-                            />
-                        </div>
-                        <div className='md:hidden flex'>
-                            <Image
-                                alt='shaking'
-                                src={'/hero/mobile_feature_icon.svg'}
-                                width={17}
-                                height={17}
-                                style={{
-                                    width: "100%",
-                                    height: "auto"
-                                }}
-                            />
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <div key={2} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between w-full'>
-                {items && items.map((item, index) => (
-                    <div key={`left-${index}`} className='flex gap-2 text-2xl items-center justify-evenly'>
-                        <p className='text-nowrap'>{item}</p>
-                        <div className='h-1/2 md:block hidden'>
-                            <Image
-                                alt='shaking'
-                                src={'/hero/Mobile_shaking.svg'}
-                                width={17}
-                                height={17}
-                                style={{
-                                    width: "auto",
-                                    height: "100%"
-                                }}
-                            />
-                        </div>
-                        <div className='md:hidden flex'>
-                            <Image
-                                alt='shaking'
-                                src={'/hero/mobile_feature_icon.svg'}
-                                width={17}
-                                height={17}
-                                style={{
-                                    width: "100%",
-                                    height: "auto"
-                                }}
-                            />
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <div key={3} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between w-full'>
-                {items && items.map((item, index) => (
-                    <div key={`left-${index}`} className='flex gap-2 text-2xl items-center justify-evenly'>
-                        <p className='text-nowrap'>{item}</p>
-                        <div className='h-1/2 md:block hidden'>
-                            <Image
-                                alt='shaking'
-                                src={'/hero/Mobile_shaking.svg'}
-                                width={17}
-                                height={17}
-                                style={{
-                                    width: "auto",
-                                    height: "100%"
-                                }}
-                            />
-                        </div>
-                        <div className='md:hidden flex'>
-                            <Image
-                                alt='shaking'
-                                src={'/hero/mobile_feature_icon.svg'}
-                                width={17}
-                                height={17}
-                                style={{
-                                    width: "100%",
-                                    height: "auto"
-                                }}
-                            />
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <div key={4} className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between w-full'>
-                {items && items.map((item, index) => (
-                    <div key={`left-${index}`} className='flex gap-2 text-2xl items-center justify-evenly'>
+            <div className='md:bg-primary border-none bg-primary-trans md:text-white text-primary flex gap-2 justify-between'>
+                {duplicatedItems.map((item, index) => (
+                    <div key={item} className='flex gap-2 text-2xl items-center justify-evenly'>
                         <p className='text-nowrap'>{item}</p>
                         <div className='h-1/2 md:block hidden'>
                             <Image
