eeb9c7293384be1047cab9ef8cbf58195d29f5ff
wuubangdev <wuubangdev@gmail.com>
2024-10-22 18:03:44 +0700

    add show case hero button

--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -11,7 +11,6 @@ const HeroSection = () => {
     const videoHeight = useTransform(scrollYProgress, [0, 0.05], ['100%', '200%']);
     const translateY = useTransform(scrollYProgress, [0, 0.05], ['0%', '70%']);
     const videoPadding = useTransform(scrollYProgress, [0, 0.05], [16, 0]);
-
     const translateYF = useTransform(scrollYProgress, [0, 0.05], ['0', '60vh']);
     const translateYSearch = useTransform(scrollYProgress, [0, 0.06], ['0', '60vh']);
     const opacity = useTransform(scrollYProgress, [0, 0.05], [1, 0]);
@@ -47,16 +46,18 @@ const HeroSection = () => {
                         <motion.div
                             className='absolute bottom-4 right-6 flex items-center rounded-[41px]
                             p-4 gap-2 text-white bg-black-blur cursor-pointer border-3-regular z-50 group
-                             hover:bg-black hover:w-[220px] w-auto duration-500 transition-all'
+                             hover:bg-black w-14 hover:w-[220px] duration-500 transition-all overflow-hidden'
                             style={{
                                 translateY: translateYSearch,
                                 // background: 'black',
                             }}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                            </svg>
-                            <span className='hidden group-hover:block' >View this showcase</span>
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                                </svg>
+                            </div>
+                            <span className='group-hover:scale-100 scale-0 duration-500 origin-right text-white text-nowrap' >View this showcase</span>
                         </motion.div>
                     </div>
                     <div className='flex flex-col justify-between text-primary'>
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -50,15 +50,18 @@ const Hero = () => {
                         <motion.div
                             className='absolute bottom-4 right-6 flex items-center rounded-[41px]
                             p-4 gap-2 text-white bg-black-blur cursor-pointer border-3-regular z-50 group
-                             hover:bg-black hover:w-[220px] w-auto duration-500 transition-all'
+                             hover:bg-black w-14 hover:w-[220px] duration-500 transition-all overflow-hidden'
                             style={{
                                 translateY: videoTranslateY,
+                                // background: 'black',
                             }}
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                            </svg>
-                            <span className='hidden group-hover:block' >View this showcase</span>
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                                </svg>
+                            </div>
+                            <span className='group-hover:scale-100 scale-0 duration-500 origin-right text-white text-nowrap' >View this showcase</span>
                         </motion.div>
                     </div>
 
