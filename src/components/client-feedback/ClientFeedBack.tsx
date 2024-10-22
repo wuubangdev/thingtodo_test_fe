95ff1faa92b418efce5585dfede1e6a2864b9b14
wuubangdev <wuubangdev@gmail.com>
2024-10-22 14:09:17 +0700

    fix add link footer

--- src/components/client-feedback/ClientFeedBack.tsx
+++ src/components/client-feedback/ClientFeedBack.tsx
@@ -62,25 +62,73 @@ const ClientFeedBack: React.FC = () => {
                         </span>
                     </div>
                 </div>
-                <div className='flex gap-6 items-center pr-3'>
-                    <Image
-                        alt='feedback-arrow-left'
-                        src={'/feedback/arrow_right.svg'}
-                        width={28}
-                        height={20}
-                        className='h-full w-auto rotate-180 translate-x-full cursor-pointer hover:scale-x-150 origin-left duration-500'
+                <div className='flex gap-2 items-center'>
+                    <div
+                        className='flex items-center group h-full cursor-pointer rotate-180 relative'
                         onClick={() => sliderRef.current?.slickPrev()}
-                        loading="lazy"
-                    />
-                    <Image
-                        alt='feedback-arrow-left'
-                        src={'/feedback/arrow_right.svg'}
-                        width={28}
-                        height={20}
-                        className='h-full w-auto cursor-pointer hover:scale-x-150 origin-left duration-500'
+                    >
+                        <div className='absolute right-0 -translate-x-1 scale-x-150'>
+                            <svg width="10" height="21" viewBox="0 0 10 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clipPath="url(#clip0_2060_990)">
+                                    <path
+                                        d="M-27.7681 9.93218V11.0994L-18.3675 20.5L-17.295 19.3959L-25.3076 11.4306V11.2886L10 11.2886V9.71136L-25.3076 9.71136V9.58517L-17.295 1.6041L-18.3675 0.5L-27.7681 9.93218Z"
+                                        fill="#FCFCFC" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_2060_990">
+                                        <rect width="10" height="20" fill="white" transform="matrix(-1 0 0 1 10 0.5)" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </div>
+                        <div className='group-hover:translate-x-4 translate-x-2 duration-500'>
+                            <svg width="28" height="21" viewBox="0 0 28 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clipPath="url(#clip0_2060_996)">
+                                    <path
+                                        d="M27.7681 9.93218V11.0994L18.3675 20.5L17.295 19.3959L25.3076 11.4306V11.2886L10 11.2886V9.71136L25.3076 9.71136V9.58517L17.295 1.6041L18.3675 0.5L27.7681 9.93218Z"
+                                        fill="#FCFCFC" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_2060_996">
+                                        <rect width="28" height="20" fill="white" transform="translate(0 0.5)" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </div>
+                    </div>
+                    <div
+                        className='flex items-center group h-full cursor-pointer relative'
                         onClick={() => sliderRef.current?.slickNext()}
-                        loading="lazy"
-                    />
+                    >
+                        <div className='absolute left-0 group-hover:scale-x-150 origin-right duration-500'>
+                            <svg width="10" height="21" viewBox="0 0 10 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clipPath="url(#clip0_2060_990)">
+                                    <path
+                                        d="M-27.7681 9.93218V11.0994L-18.3675 20.5L-17.295 19.3959L-25.3076 11.4306V11.2886L10 11.2886V9.71136L-25.3076 9.71136V9.58517L-17.295 1.6041L-18.3675 0.5L-27.7681 9.93218Z"
+                                        fill="#FCFCFC" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_2060_990">
+                                        <rect width="10" height="20" fill="white" transform="matrix(-1 0 0 1 10 0.5)" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </div>
+                        <div className=' duration-500'>
+                            <svg width="28" height="21" viewBox="0 0 28 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clipPath="url(#clip0_2060_996)">
+                                    <path
+                                        d="M27.7681 9.93218V11.0994L18.3675 20.5L17.295 19.3959L25.3076 11.4306V11.2886L10 11.2886V9.71136L25.3076 9.71136V9.58517L17.295 1.6041L18.3675 0.5L27.7681 9.93218Z"
+                                        fill="#FCFCFC" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_2060_996">
+                                        <rect width="28" height="20" fill="white" transform="translate(0 0.5)" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div>
