c864fa23a06222e735d2badaff1821efa6aaeba4
kingf <kingf@PAUL>
2024-10-31 00:56:34 +0700

    fix bug

--- src/components/navbar/NavbarError.tsx
+++ src/components/navbar/NavbarError.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react';
-import Menu from './Menu';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useMobileMenuContext } from '../context/MobileMenuContext';
