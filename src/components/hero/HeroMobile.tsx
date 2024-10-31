e36251c3417fc981b0c47d32bc05a88b72a81588
wuubangdev <wuubangdev@gmail.com>
2024-11-01 06:33:22 +0700

    add video

--- src/components/hero/HeroMobile.tsx
+++ src/components/hero/HeroMobile.tsx
@@ -77,8 +77,8 @@ const HeroMobile = () => {
                                      hover:bg-black hover:w-[220px] w-10 h-10 duration-500 transition-all overflow-hidden'
                         onClick={() => handleClick()}
                     >
-                        <div className='absolute'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                        <div className='absolute group-hover:left-3'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                             </svg>
                         </div>
