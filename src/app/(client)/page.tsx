e25e18e09001c527715c181a5116513e7f82d130
wuubangdev <wuubangdev@gmail.com>
2024-10-31 09:03:35 +0700

    close client feedback

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,4 +1,4 @@
-import ClientFeedBack from "@/components/client-feedback/ClientFeedBack";
+// import ClientFeedBack from "@/components/client-feedback/ClientFeedBack";
 import Feature from "@/components/feature/Feature";
 import Footer from "@/components/footer/Footer";
 import HeroSection from "@/components/hero-section/HeroSection";
@@ -105,7 +105,7 @@ export default function Home() {
           }}
         >
           <OurValued />
-          <ClientFeedBack />
+          {/* <ClientFeedBack /> */}
         </div>
         <Footer />
       </div>
