88dd9d217e839a8b1b538d4d6b810dc4cc6c1949
wuubangdev <wuubangdev@gmail.com>
2024-10-31 14:43:41 +0700

    fix navbar

--- src/components/hero/HeroMobile.tsx
+++ src/components/hero/HeroMobile.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import HeroTextMobile from './HeroTextMobile'
 import { useScroll, useTransform, motion } from 'framer-motion';
 import Feature from '../feature/Feature';
@@ -11,8 +11,9 @@ const HeroMobile = () => {
     const padding = useTransform(scrollYProgress, [0, 0.02], ['16px', '0px']);
     const videoHeight = useTransform(scrollYProgress, [0, 0.03, 0.08, 0.2], ['100%', '200%', '260%', '260%']);
     const videoWidth = useTransform(scrollYProgress, [0, 0.03, 0.08, 0.2], ['100%', '100%', '130%', '130%']);
-    const translate = useTransform(scrollYProgress, [0, 0.04, 0.08], ['0px', '0px', '28px']);
-    const translateButton = useTransform(scrollYProgress, [0, 0.06, 0.08], ['0px', '60vh', '63vh']);
+    const translate = useTransform(scrollYProgress, [0, 0.04, 0.08], ['0px', '0px', '32px']);
+    const translateF = useTransform(scrollYProgress, [0, 0.04, 0.08], ['0px', '100px', '200px']);
+    const opacity = useTransform(scrollYProgress, [0, 0.01], ['1px', '0']);
     const radius = useTransform(scrollYProgress, [0, 0.03], ['40px', '0px']);
     const containerHeight = containerRef.current?.offsetHeight || 0;
 
@@ -24,22 +25,23 @@ const HeroMobile = () => {
         [0, containerHeight / 2] // Giảm giá trị này để làm cuộn chậm trong khoảng này
     );
 
-    const [checkClick, setCheckClick] = useState<number>(1);
+    // const [checkClick, setCheckClick] = useState<number>(1);
 
-    const handleClick = () => {
-        // Mở link trong tab mới
-        if (checkClick === 1) {
-            setCheckClick(checkClick + 1);
-        }
-        if (checkClick === 2) {
-            setCheckClick(checkClick - 1);
-            window.open('https://fb.watch/vyHzMqrSNM/', '_blank');
-        }
-    };
+
+    // const handleClick = () => {
+    //     // Mở link trong tab mới
+    //     if (checkClick === 1) {
+    //         setCheckClick(checkClick + 1);
+    //     }
+    //     if (checkClick === 2) {
+    //         setCheckClick(checkClick - 1);
+    //         window.open('https://fb.watch/vyHzMqrSNM/', '_blank');
+    //     }
+    // };
     return (
         <div
             ref={containerRef}
-            className='flex flex-col md:hidden bg-primary h-[calc(100vh-72px)] mb-[60vh]'
+            className='flex flex-col md:hidden bg-primary h-[calc(100vh-72px)] mb-[58vh]'
         >
             <HeroTextMobile />
             <motion.div className='w-full flex-1 flex flex-col relative'
@@ -74,13 +76,13 @@ const HeroMobile = () => {
                         </motion.video>
                     </div>
                 </div>
-                <motion.div
+                {/* <motion.div
                     className='absolute bottom-[76px] right-7 flex items-center rounded-[41px]
-                            p-4 gap-2 text-white bg-black-blur cursor-pointer border-3-regular z-50 group
-                             hover:bg-black w-14 hover:w-[220px] duration-500 transition-all overflow-hidden'
+                        p-4 gap-2 text-white bg-black-blur cursor-pointer border-3-regular z-10 group
+                         hover:bg-black w-14 hover:w-[220px] duration-500 transition-all overflow-hidden'
                     onClick={() => handleClick()}
                     style={{
-                        translateY: translateButton,
+                        display: btnDisplay
                     }}
                 >
                     <div>
@@ -89,8 +91,15 @@ const HeroMobile = () => {
                         </svg>
                     </div>
                     <span className='group-hover:scale-100 scale-0 duration-500 origin-right text-white text-nowrap' >View this showcase</span>
+                </motion.div> */}
+                <motion.div
+                    style={{
+                        translateY: translateF,
+                        opacity: opacity,
+                    }}
+                >
+                    <Feature />
                 </motion.div>
-                <Feature />
             </motion.div>
         </div>
     )
