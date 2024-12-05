66728966f748b593c74e511b523df7b871d58c79
wuubangdev <wuubangdev@gmail.com>
2024-12-05 17:17:01 +0700

    update project details api

--- src/components/project-detail/ProjectList.tsx
+++ src/components/project-detail/ProjectList.tsx
@@ -5,7 +5,6 @@ interface IProps {
     projectData: Product[];
 }
 
-
 const renderProductList: React.FC<Product> = ({ id, title, fileList }) => {
     if (fileList.length === 1) {
         return (
@@ -22,7 +21,7 @@ const renderProductList: React.FC<Product> = ({ id, title, fileList }) => {
     }
     if (fileList.length === 2) {
         return (
-            <div key={id} className='flex flex-col gap-6'>
+            <div key={id} className='flex flex-col gap-16'>
                 <div className='grid grid-cols-2 justify-end'>
                     {fileList && fileList.map((item, index) => (
                         <RenderMedia key={index} media={item} />
@@ -37,8 +36,8 @@ const renderProductList: React.FC<Product> = ({ id, title, fileList }) => {
     }
     if (fileList.length === 3) {
         return (
-            <div key={id} className='flex flex-col gap-6'>
-                <div className='grid grid-cols-1 gap-6'>
+            <div key={id} className='flex flex-col gap-16'>
+                <div className='grid grid-cols-1 gap-16'>
                     <div className='grid grid-cols-2 gap-6'>
                         <RenderMedia media={fileList[0]} />
                         <RenderMedia media={fileList[1]} />
@@ -54,15 +53,54 @@ const renderProductList: React.FC<Product> = ({ id, title, fileList }) => {
             </div>
         )
     }
+    if (fileList.length === 4) {
+        return (
+            <div key={id} className='flex flex-col gap-16'>
+                <div className='grid grid-cols-1 gap-16'>
+                    <div className='grid grid-cols-2 gap-6'>
+                        <RenderMedia media={fileList[0]} />
+                        <RenderMedia media={fileList[1]} />
+                        <RenderMedia media={fileList[2]} />
+                        <RenderMedia media={fileList[3]} />
+                    </div>
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-2'>
+                    <div><p className='body-2-regular'>{title}</p></div>
+                    <div></div>
+                </div>
+            </div>
+        )
+    }
+    if (fileList.length === 5) {
+        return (
+            <div key={id} className='flex flex-col gap-16'>
+                <div className='grid grid-cols-1 gap-6'>
+                    <div className='grid grid-cols-2 gap-16'>
+                        <RenderMedia media={fileList[0]} />
+                        <RenderMedia media={fileList[1]} />
+                        <RenderMedia media={fileList[2]} />
+                        <RenderMedia media={fileList[3]} />
+                    </div>
+                    <div className='grid grid-cols-1'>
+                        <RenderMedia media={fileList[4]} />
+                    </div>
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-2'>
+                    <div><p className='body-2-regular'>{title}</p></div>
+                    <div></div>
+                </div>
+            </div>
+        )
+    }
 }
 
 const ProjectList: React.FC<IProps> = ({ projectData }) => {
     return (
-        <>
+        <div className='grid grid-cols-1 gap-16'>
             {projectData && projectData.map(item => (
                 renderProductList({ title: item.title, fileList: item.fileList, id: item.id })
             ))}
-        </>
+        </div>
     )
 }
 
