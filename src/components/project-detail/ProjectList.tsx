6b1fa9c08f387af9e4a594f1f90485fee546974d
wuubangdev <wuubangdev@gmail.com>
2024-12-11 15:31:44 +0700

    update product list show

--- src/components/project-detail/ProjectList.tsx
+++ src/components/project-detail/ProjectList.tsx
@@ -22,10 +22,9 @@ const renderProductList: React.FC<Product> = ({ id, title, fileList }) => {
     if (fileList.length === 2) {
         return (
             <div key={id} className='flex flex-col gap-16'>
-                <div className='grid grid-cols-2 justify-end'>
-                    {fileList && fileList.map((item, index) => (
-                        <RenderMedia key={index} media={item} />
-                    ))}
+                <div className='grid grid-cols-1 gap-16'>
+                    <RenderMedia media={fileList[1]} />
+                    <RenderMedia media={fileList[0]} />
                 </div>
                 <div className='grid grid-cols-1 md:grid-cols-2'>
                     <div><p className='body-2-regular'>{title}</p></div>
@@ -39,11 +38,11 @@ const renderProductList: React.FC<Product> = ({ id, title, fileList }) => {
             <div key={id} className='flex flex-col gap-16'>
                 <div className='grid grid-cols-1 gap-16'>
                     <div className='grid grid-cols-2 gap-6'>
-                        <RenderMedia media={fileList[0]} />
+                        <RenderMedia media={fileList[2]} />
                         <RenderMedia media={fileList[1]} />
                     </div>
                     <div className='grid grid-cols-1'>
-                        <RenderMedia media={fileList[2]} />
+                        <RenderMedia media={fileList[0]} />
                     </div>
                 </div>
                 <div className='grid grid-cols-1 md:grid-cols-2'>
@@ -58,10 +57,10 @@ const renderProductList: React.FC<Product> = ({ id, title, fileList }) => {
             <div key={id} className='flex flex-col gap-16'>
                 <div className='grid grid-cols-1 gap-16'>
                     <div className='grid grid-cols-2 gap-6'>
-                        <RenderMedia media={fileList[0]} />
-                        <RenderMedia media={fileList[1]} />
-                        <RenderMedia media={fileList[2]} />
                         <RenderMedia media={fileList[3]} />
+                        <RenderMedia media={fileList[2]} />
+                        <RenderMedia media={fileList[1]} />
+                        <RenderMedia media={fileList[0]} />
                     </div>
                 </div>
                 <div className='grid grid-cols-1 md:grid-cols-2'>
@@ -76,13 +75,13 @@ const renderProductList: React.FC<Product> = ({ id, title, fileList }) => {
             <div key={id} className='flex flex-col gap-16'>
                 <div className='grid grid-cols-1 gap-6'>
                     <div className='grid grid-cols-2 gap-16'>
-                        <RenderMedia media={fileList[0]} />
-                        <RenderMedia media={fileList[1]} />
-                        <RenderMedia media={fileList[2]} />
+                        <RenderMedia media={fileList[4]} />
                         <RenderMedia media={fileList[3]} />
+                        <RenderMedia media={fileList[2]} />
+                        <RenderMedia media={fileList[1]} />
                     </div>
                     <div className='grid grid-cols-1'>
-                        <RenderMedia media={fileList[4]} />
+                        <RenderMedia media={fileList[0]} />
                     </div>
                 </div>
                 <div className='grid grid-cols-1 md:grid-cols-2'>
